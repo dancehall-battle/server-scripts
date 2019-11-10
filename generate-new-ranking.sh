bfb89a9130bd4aa9019c7c9a9e0f16b467e01293
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-11-10 17:55:47 +0100

    updat ldf config

--- generate-new-ranking.sh
+++ generate-new-ranking.sh
@@ -2,6 +2,8 @@
 
 ROOTDIR=$1
 LDFSERVERID=$2
+CONFIG=$3
+DATASOURCE=$4
 
 if [ "$ROOTDIR" = "" ]
 then
@@ -10,6 +12,7 @@ then
 fi
 
 RANKING="../ranking/bin/cli.js"
+UPDATECONFIG="node ../ranking/scripts/update-ldf-config.js -c $CONFIG -d $DATASOURCE -f "
 
 OPTIONS_DANCER_1VS1="-p 1 -f -j"
 OPTIONS_DANCER_2VS2="-p 2 -f -j"
@@ -18,16 +21,33 @@ OPTIONS_COUNTRY_BOTH="-r country -j"
 OPTIONS_COUNTRY_HOME="-r country -h -j"
 OPTIONS_COUNTRY_AWAY="-r country -a -j"
 
-DIR=$ROOTDIR/$(date +%Y-%m-%d);
+TODAY=$(date +%Y-%m-%d);
+DIR=$ROOTDIR/$TODAY;
 
 mkdir $DIR
 
 # Generate rankings for dancers
 $RANKING $OPTIONS_DANCER_1VS1 > $DIR/dancer-1vs1.jsonld
+$UPDATECONFIG $DIR/dancer-1vs1.jsonld -t "$TODAY-dancer-1vs1" > temp.jsonld
+mv temp.jsonld $CONFIG
+
 $RANKING $OPTIONS_DANCER_2VS2 > $DIR/dancer-2vs2.jsonld
+$UPDATECONFIG $DIR/dancer-2vs2.jsonld -t "$TODAY-dancer-2vs2" > temp.jsonld
+mv temp.jsonld $CONFIG
+
 $RANKING $OPTIONS_DANCER_COMBINED > $DIR/dancer-combined.jsonld
+$UPDATECONFIG $DIR/dancer-combined.jsonld -t "$TODAY-dancer-combined" > temp.jsonld
+mv temp.jsonld $CONFIG
 
 # Generate rankings for countries
 $RANKING $OPTIONS_COUNTRY_BOTH > $DIR/country-both.jsonld
+$UPDATECONFIG $DIR/country-both.jsonld -t "$TODAY-country-both" > temp.jsonld
+mv temp.jsonld $CONFIG
+
 $RANKING $OPTIONS_COUNTRY_HOME > $DIR/country-home.jsonld
+$UPDATECONFIG $DIR/country-home.jsonld -t "$TODAY-country-home" > temp.jsonld
+mv temp.jsonld $CONFIG
+
 $RANKING $OPTIONS_COUNTRY_AWAY > $DIR/country-away.jsonld
+$UPDATECONFIG $DIR/country-away.jsonld -t "$TODAY-country-away" > temp.jsonld
+mv temp.jsonld $CONFIG
