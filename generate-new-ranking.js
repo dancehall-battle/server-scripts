15bd44a3c203b91f554eec1e31e5bd321a1f15ce
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-11-17 20:05:21 +0100

    write rankings to files

--- generate-new-ranking.js
+++ generate-new-ranking.js
@@ -2,7 +2,7 @@ const program = require('commander');
 const pkg = require('./package');
 const path = require('path');
 const {format} = require('date-fns');
-const ranking = require('dhb-ranking');
+const {DancerRanker, CountryRanker} = require('dhb-ranking');
 const fs = require('fs-extra');
 
 program
@@ -25,7 +25,70 @@ const directory = path.join(rootDirectory, today);
 
 fs.ensureDirSync(directory);
 
-
 if (!path.isAbsolute(program.config)) {
   program.config = path.resolve(process.cwd(), program.config);
+}
+
+const todayDate = new Date();
+const oneYearAgo = new Date();
+oneYearAgo.setFullYear(todayDate.getFullYear() - 1);
+
+generateRanking(new DancerRanker,{
+  participants: ['1'],
+  startDate: oneYearAgo,
+  endDate: todayDate,
+  format: 'jsonld',
+  homeAway: 'both',
+  removeFemaleBattles: true
+}, `dancer-1vs1.jsonld`);
+
+generateRanking(new DancerRanker,{
+  participants: ['2'],
+  startDate: oneYearAgo,
+  endDate: todayDate,
+  format: 'jsonld',
+  homeAway: 'both',
+  removeFemaleBattles: true
+}, `dancer-2vs2.jsonld`);
+
+generateRanking(new DancerRanker,{
+  participants: ['1', '2'],
+  startDate: oneYearAgo,
+  endDate: todayDate,
+  format: 'jsonld',
+  homeAway: 'both',
+  removeFemaleBattles: true
+}, `dancer-combined.jsonld`);
+
+generateRanking(new CountryRanker,{
+  participants: ['1', '2'],
+  startDate: oneYearAgo,
+  endDate: todayDate,
+  format: 'jsonld',
+  homeAway: 'both'
+}, `country-both.jsonld`);
+
+generateRanking(new CountryRanker,{
+  participants: ['1', '2'],
+  startDate: oneYearAgo,
+  endDate: todayDate,
+  format: 'jsonld',
+  homeAway: 'home'
+}, `country-home.jsonld`);
+
+generateRanking(new CountryRanker,{
+  participants: ['1', '2'],
+  startDate: oneYearAgo,
+  endDate: todayDate,
+  format: 'jsonld',
+  homeAway: 'away',
+  scale: true
+}, `country-away.jsonld`);
+
+async function generateRanking(ranker, options, filename) {
+  const result = await ranker.getRanking(options);
+
+  fs.writeFile(path.resolve(directory, filename), JSON.stringify(result), (err) => {
+    if (err) throw err;
+  });
 }
